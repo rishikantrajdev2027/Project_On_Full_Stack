240854dd9586892a6ce09900be951ba71bfc01a1
Rishi Kant Raj <rishikantiit2003@gmail.com>
2026-08-30 11:08:06 +0530

    add cloudinary and middleware

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,12 +3,12 @@ class ApiError extends Error {
         statusCode, 
         message= "Something went wrong",
         errors =[],
-        statck = ""
+        stack = ""
     ){
         super(message)
         this.statusCode = statusCode
         this.data = null
-        this,message = message
+        this.message = message
         this.success = false;
         this.errors = errors
 
