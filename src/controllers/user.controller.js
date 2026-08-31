f01896812eacb90e09a2cc9a9d51213fdd55b41b
Rishi Kant Raj <rishikantiit2003@gmail.com>
2026-08-31 19:05:12 +0530

    add

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -26,7 +26,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All field are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
